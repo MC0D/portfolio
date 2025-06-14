acf1ce05fd2f073478b512ae32eeb68051949267
MC0D <mik.fdc@hotmail.fr>
2025-06-14 18:12:15 +0200

    Mise en place du menu burger pour mobile / amélioration du responsive

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
-const form = document.getElementById("contact-form");
-const formSend = document.getElementById("form-message");
+document.addEventListener("DOMContentLoaded", () => {
+  const form = document.getElementById("contact-form");
+  const formSend = document.getElementById("form-message");
 
-form.addEventListener("submit", (e) => {
-  e.preventDefault();
-  formSend.textContent = "Merci ! Votre message à bien été envoyé.";
+  if (form && formSend) {
+    form.addEventListener("submit", (e) => {
+      e.preventDefault();
+      formSend.textContent = "Merci ! Votre message à bien été envoyé.";
+    });
+  }
+
+  const burger = document.querySelector(".burger");
+  const menu = document.querySelector(".menu");
+
+  burger.addEventListener("click", () => {
+    menu.classList.toggle("active");
+  });
 });
