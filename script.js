cc1c9d9d1a75052a03bdc5f4316b3bdebea49b66
MC0D <mik.fdc@hotmail.fr>
2025-06-14 15:41:45 +0200

    Petite refactorisation du code / debug actualisation page après envoie du formulaire

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const form = document.getElementById("contact-form");
 const formSend = document.getElementById("form-message");
 
-form.addEventListener("submit", () => {
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
   formSend.textContent = "Merci ! Votre message à bien été envoyé.";
 });
